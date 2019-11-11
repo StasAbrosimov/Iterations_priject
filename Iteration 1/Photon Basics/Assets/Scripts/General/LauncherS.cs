8bbc31210bc26c38789f820ecff3da5c86637c19
sabrosimovpa <s.abrosimov@program-ace.com.ua>
2019-11-11 19:05:46 +0200

    Photon

---
+++ Iteration 1/Photon Basics/Assets/Scripts/General/LauncherS.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LauncherS : MonoBehaviourPunCallbacks
+{
+    #region Serialization Fields
+    [Tooltip("GameVersion")]
+    [SerializeField]
+    private string gameVersion = "1";
+
+    /// <summary>
+    /// The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.
+    /// </summary>
+    [Tooltip("The maximum number of players per room.")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    
+
+    #endregion
+
+    #region Private fields
+
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region MonoBehaviourPunCallbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log(string.Format("OnDisconnected by cause: {0}", cause.ToString()));
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = this.maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+    }
+
+    #endregion
+
+    #region Publick Methods
+    public void Connect()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+    #endregion
+}
---
+++ Iteration 1/Photon Basics/Assets/Scripts/Player/PlayerNameInputField_S.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField_S : MonoBehaviour
+{
+
+    const string playerNamePrefKey = "PlayerName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defName = string.Empty;
+        InputField inputF = this.GetComponent<InputField>();
+        if(inputF != null)
+        {
+            if(PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defName = PlayerPrefs.GetString(playerNamePrefKey);
+                inputF.text = defName;
+            }
+        }
+        else
+        {
+            throw new MissingComponentException(typeof(InputField).ToString());
+        }
+    }
+
+    public void SetPlayerName(string newName)
+    {
+        if(string.IsNullOrEmpty(newName))
+        {
+            Debug.Log("newName is empty");
+        }
+        else
+        {
+            Photon.Pun.PhotonNetwork.NickName = newName;
+            Debug.Log(string.Format("newName is \"{0}\"", newName));
+            PlayerPrefs.SetString(playerNamePrefKey, newName);
+            PlayerPrefs.Save();
+        }
+    }
+}
